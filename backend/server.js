cc94881875642446b84a63267d3ba94cc8e910ab
Vivek Mogalapalli <vimogala@microsoft.com>
2020-07-27 15:36:52 -0700

    Add prettier

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: __dirname });
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
 });
 
 const { PORT } = process.env;
