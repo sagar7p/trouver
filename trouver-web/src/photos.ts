52450be290e6e6282ce45e5603b79bc1f9cf61f8
Rishabh Saxena <risaxena@microsoft.com>
2020-07-27 13:34:50 -0700

    Add Grid Gallery and web app

---
+++ trouver-web/src/photos.ts
@@ -0,0 +1,13 @@
+export const photos: string[] = [
+    'https://source.unsplash.com/1600x900?nature',
+    'https://source.unsplash.com/1600x900?food',
+    'https://source.unsplash.com/1600x900?fitness',
+    'https://source.unsplash.com/1600x900?cars',
+    'https://source.unsplash.com/1600x900?water',
+    'https://source.unsplash.com/1600x900?movies',
+    'https://source.unsplash.com/1600x900?photography',
+    'https://source.unsplash.com/1600x900?random',
+    'https://source.unsplash.com/1600x900?volcano',
+    'https://source.unsplash.com/1600x900?fish',
+    'https://source.unsplash.com/1600x900?mountain'
+]
