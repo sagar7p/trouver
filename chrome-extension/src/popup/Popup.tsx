f2fd03f4b7893977bfeb5f3f76ba9d1f1cbc50d9
Sagar Punhani <sagar7p@gmail.com>
2020-07-28 13:38:09 -0700

    built submission form

--- chrome-extension/src/popup/Popup.tsx
+++ chrome-extension/src/popup/Popup.tsx
@@ -1,24 +1,86 @@
-import React, { Component, RefObject } from 'react'; // let's also import Component
+import React, { Component, RefObject } from "react"; // let's also import Component
 import "./Popup.scss";
-type FormProps = {
-  imageUrl: string
+
+interface Place {
+  userId: string;
+  name: string;
+  lat: number;
+  long: number;
+  imageUrl: string;
+  imageHeight: number;
+  imageWidth: number;
 }
-export default class Popup extends Component<FormProps> {
+interface FormProps {
+  imageUrl: string;
+}
+
+interface FormState {
+  place?: google.maps.places.PlaceResult;
+  imgHeight: number;
+  imgWidth: number;
+}
+
+export default class Popup extends Component<FormProps, FormState> {
+  constructor(props) {
+    super(props);
+    this.state = { place: null, imgHeight: 0, imgWidth: 0 };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.onImgLoad = this.onImgLoad.bind(this);
+  }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h1 id="title">Save your location</h1>
         <p id="subtitle">Enter this location:</p>
-        <SearchBar/>
-        <img src={this.props.imageUrl} alt="Logo" id="image"/>
+        <SearchBar onPlaceLoaded={this.onPlaceLoaded.bind(this)} />
+        <img
+          src={this.props.imageUrl}
+          alt="Logo"
+          id="image"
+          onLoad={this.onImgLoad}
+        />
+        <input id="submit" type="submit" value="Submit" />
       </form>
     );
   }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { place, imgHeight, imgWidth } = this.state;
+    const payload: Place = {
+      userId: "1",
+      name: place.name,
+      lat: place.geometry.location.lat(),
+      long: place.geometry.location.lng(),
+      imageUrl: this.props.imageUrl,
+      imageHeight: imgHeight,
+      imageWidth: imgWidth,
+    };
+
+    console.log("submitted " + JSON.stringify(payload));
+
+    return false;
+  }
+
+  onPlaceLoaded(place) {
+    this.setState({ place: place }, () => {
+      console.log("place picked " + this.state.place.name);
+    });
+  }
+
+  onImgLoad({ target: img }) {
+    console.log(img.offsetHeight);
+    console.log(img.offsetWidth);
+    this.setState({
+      imgHeight: img.offsetHeight,
+      imgWidth: img.offsetWidth,
+    });
+  }
 }
 
-type SearchBarProps = {
-  onPlaceLoaded?: (place: google.maps.places.PlaceResult) => {}
+interface SearchBarProps {
+  onPlaceLoaded?: (place: google.maps.places.PlaceResult) => {};
 }
 
 class SearchBar extends React.Component<SearchBarProps> {
@@ -33,27 +95,27 @@ class SearchBar extends React.Component<SearchBarProps> {
   }
 
   componentDidMount() {
-    this.autocomplete = new google.maps.places.Autocomplete(this.autocompleteInput.current,
-        {});
+    this.autocomplete = new google.maps.places.Autocomplete(
+      this.autocompleteInput.current,
+      {}
+    );
 
-    this.autocomplete.addListener('place_changed', this.handlePlaceChanged);
+    this.autocomplete.addListener("place_changed", this.handlePlaceChanged);
   }
 
-  handlePlaceChanged(){
+  handlePlaceChanged() {
     const place = this.autocomplete.getPlace();
     this.props.onPlaceLoaded(place);
   }
 
   render() {
     return (
-        <input 
-          ref={this.autocompleteInput}
-          id="autocomplete" 
-          placeholder="Enter your address"
-          type="Enter a location">
-        </input>
+      <input
+        ref={this.autocompleteInput}
+        id="autocomplete"
+        placeholder="Enter your address"
+        type="Enter a location"
+      ></input>
     );
   }
 }
-
-
--- chrome-extension/src/popup/index.tsx
+++ chrome-extension/src/popup/index.tsx
@@ -3,7 +3,6 @@ import * as ReactDOM from "react-dom";
 import Popup from "./Popup";
 
 chrome.tabs.query({ active: true, currentWindow: true }, (tab) => {
-
   chrome.storage.sync.get(['imageUrl'], function(result) {
       ReactDOM.render(<Popup imageUrl={result.imageUrl}/>, document.getElementById("popup"));
   });
