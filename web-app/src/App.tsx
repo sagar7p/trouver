e81628e1ce68c06ac04f83531809ab861a3e58d4
Sagar Punhani <sagar7p@gmail.com>
2020-07-29 16:54:19 -0700

    Adding google flow

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import './App.css';
-import { GridGalleryContainer } from './components/GridGallery/GridGalleryContainer';
-import { Header, currentPage } from './components/Header/Header';
-import { MapViewContainer } from './components/MapView/MapViewContainer';
-import { PageType } from './models/PageType';
-import { Place } from './models/Place';
-import { places } from './MockData';
-import { BehaviorSubject } from 'rxjs';
-import { DetailModal } from './components/DetailedView/DetailModal';
-import { PlacesService } from './services/places.service';
+import React from "react";
+import "./App.css";
+import { GridGalleryContainer } from "./components/GridGallery/GridGalleryContainer";
+import { Header, currentPage } from "./components/Header/Header";
+import { MapViewContainer } from "./components/MapView/MapViewContainer";
+import { PageType } from "./models/PageType";
+import { Place } from "./models/Place";
+import { BehaviorSubject } from "rxjs";
+import { DetailModal } from "./components/DetailedView/DetailModal";
+import { PlacesService } from "./services/places.service";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { currentId } from "./components/GoogleButton";
 
-export interface AppProps {
-}
+export interface AppProps {}
 
 export interface AppState {
   currentPage: PageType;
@@ -35,51 +34,62 @@ export interface DetailViewData {
 
 export const enableDetailView = new BehaviorSubject<DetailViewData>({
   enableDetailView: false,
-  detailViewPlace: undefined
+  detailViewPlace: undefined,
 });
 
 export const loadedPlaces = new BehaviorSubject<LoadPlacesData>({
   completed: false,
-  placesArray: []
+  placesArray: [],
 });
 
 class App extends React.Component<AppProps, AppState> {
   constructor(props: AppProps) {
     super(props);
     this.state = {
-      currentPage: PageType.GridView,
+      currentPage: PageType.LandingView,
       placeObjects: [],
       showDetailView: false,
       detailViewPlace: undefined,
-      isLoading: true
+      isLoading: true,
     };
   }
 
-  async componentDidMount(): Promise<void> {
+  componentDidMount() {
     currentPage.subscribe((page: PageType) => {
       this.setState({
         ...this.state,
-        currentPage: page
-      })
+        currentPage: page,
+      });
     });
 
     enableDetailView.subscribe((data: DetailViewData) => {
       this.setState({
         ...this.state,
         showDetailView: data.enableDetailView,
-        detailViewPlace: data.detailViewPlace
-      })
+        detailViewPlace: data.detailViewPlace,
+      });
     });
 
     loadedPlaces.subscribe((loadState: LoadPlacesData) => {
       this.setState({
         ...this.state,
         isLoading: !loadState.completed,
-        placeObjects: loadState.placesArray
-      })
-    })
+        placeObjects: loadState.placesArray,
+      });
+    });
 
-    await PlacesService.getPlaces();
+    currentId.subscribe(async (userId: string | undefined) => {
+      if (userId) {
+        await PlacesService.getPlaces(userId);
+        currentPage.next(PageType.GridView);
+      } else {
+        loadedPlaces.next({
+          completed: true,
+          placesArray: [],
+        });
+        currentPage.next(PageType.LandingView);
+      }
+    });
   }
 
   render() {
@@ -93,29 +103,33 @@ class App extends React.Component<AppProps, AppState> {
               <MapViewContainer places={this.state.placeObjects} />;
             </div>
           );
+        case PageType.LandingView:
+          return <div>Landing Page</div>;
         default:
           return <GridGalleryContainer places={this.state.placeObjects} />;
       }
-    }
+    };
 
     return (
       <div className="App">
         <header className="App-header">
-          {
-            this.state.isLoading ? <div>Loading...</div>
-              : <div>
-                <Header />
+          <div>
+            <Header />
+            {this.state.isLoading ? (
+              <div>Loading...</div>
+            ) : (
+              <div>
                 {page(this.state.currentPage)}
-                <DetailModal place={this.state.detailViewPlace} showModal={this.state.showDetailView} />
+                <DetailModal
+                  place={this.state.detailViewPlace}
+                  showModal={this.state.showDetailView}
+                />
               </div>
-          }
+            )}
+          </div>
         </header>
       </div>
     );
-  }
-
-  private getPlaces() {
-    return places;
   }
 }
 
